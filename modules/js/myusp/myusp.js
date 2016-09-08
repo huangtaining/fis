3000a471abf79526e0217dd659b8bf14bfd2e578
yangqichao <qichao.yang@unspay.com>
2016-09-08 18:28:51 +0800

    change

--- modules/js/myusp/myusp.js
+++ modules/js/myusp/myusp.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2016/9/7.
  */
+//验证手机号
+var tel = 18767802354;
+
+var _reg_phone= /^0?1[3|4|5|8][0-9]\d{8}$/;
+
+if (_reg_phone.test(tel)) {
+    alert("号码正确~");
+}else{
+    alert("号码有误~");
+}
+
+//
