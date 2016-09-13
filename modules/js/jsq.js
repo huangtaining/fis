80ff77b80a9e521ae7670bc01310dac0ee45e712
yangqichao <qichao.yang@unspay.com>
2016-09-13 10:58:11 +0800

    动态口令

--- modules/js/jsq.js
+++ modules/js/jsq.js
@@ -35,14 +35,15 @@ $(".int").click(function(){
     var _result=$("#input-box").val();
     console.log(typeof _result);
     //结果输出在哪里
-    $(".jisuan").siblings("input").val(_result)
+    $(".jisuan").siblings("input").val(_result);
+    china()
 });
 
 
 $(".clea").click(function(){
-    $("#input-box").val(0)
+    $("#input-box").val(0);
     $(".jsq").hide();
-})
+});
 
 
 document.oncontextmenu=new Function("event.returnValue=false;");
@@ -230,7 +231,7 @@ function disableRefresh(evt){
     if (evt.keyCode)
     {
         if(evt.keyCode == 13){operator('result')}
-        else if(evt.keyCode == 8){input.focus();window.event.returnValue = false;operator('backspace')}
+        else if(evt.keyCode == 8){operator('backspace')}
         else if(evt.keyCode == 27){operator('clear')}
         else if(evt.keyCode == 48){typetoinput('0')}
         else if(evt.keyCode == 49){typetoinput('1')}
@@ -259,4 +260,39 @@ function disableRefresh(evt){
         else if(evt.keyCode == 109){operator('minus')}
     }
 };
+
+//    阿拉伯数字转化为中文
+function china() {
+//        哪里需要转换大小写
+    var _money = $(".money").val() || $(".money").html();
+
+    function daxiaoxie(n) {
+        var fraction = ['角', '分'];
+        var digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+        var unit = [['元', '万', '亿'], ['', '拾', '佰', '仟']];
+        var head = n < 0 ? '欠' : '';
+        n = Math.abs(n);
+        var s = '';
+        for (var i = 0; i < fraction.length; i++) {
+            s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+        }
+        s = s || '整';
+        n = Math.floor(n);
+        for (var i = 0; i < unit[0].length && n > 0; i++) {
+            var p = '';
+            for (var j = 0; j < unit[1].length && n > 0; j++) {
+                p = digit[n % 10] + unit[1][j] + p;
+                n = Math.floor(n / 10);
+            }
+            s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+        }
+        var _bigmoney = head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
+
+//              输出到哪里
+        $(".china").val(_bigmoney);
+    }
+
+    daxiaoxie(_money)
+}
+
 
