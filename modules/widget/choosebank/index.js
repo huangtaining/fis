52742795a90ace3d7fef13c51d5791a0fb146618
PeterZhu <taoqianbao@gmail.com>
2016-09-14 17:52:58 +0800

    fix active class

--- modules/widget/choosebank/index.js
+++ modules/widget/choosebank/index.js
@@ -5,12 +5,15 @@ console.log('choosebank.js');
 $(function() {
 
     $("#tabcard-cxk").bind("click", function() {
-
+        $("#tabcard-cxk").addClass("active");
+        $("#tabcard-xyk").removeClass("active");
         $("#card2").hide();
         $("#card1").show();
     });
 
     $("#tabcard-xyk").bind("click", function() {
+        $("#tabcard-cxk").removeClass("active");
+        $("#tabcard-xyk").addClass("active");
         $("#card2").show();
         $("#card1").hide();
     });
