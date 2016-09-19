efcf232de3933881c8a826a8597f18f0c2335ab1
yangqichao <qichao.yang@unspay.com>
2016-09-19 17:16:44 +0800

    js

--- modules/js/safe.js
+++ modules/js/safe.js
@@ -2,25 +2,30 @@
  * Created by Administrator on 2016/9/12.
  */
 var $ = require('jquery');
-var _st=1;
-var _gt=1;
-$(".masage thead input").eq(0).click(function(){
-    _st*=-1
-        if(_st==-1){
-            $(".masage tbody input:even").prop("checked",false);
-      }else if(_st==1){
-            $(".masage tbody input:even").prop("checked",true);
-      }
-});
-$(".masage thead input").eq(1).click(function(){
-    _gt*=-1
-    if(_gt==-1){
-        $(".masage tbody input:odd").prop("checked",false);
-    }else if (_gt==1){
-        $(".masage tbody input:odd").prop("checked",true);
-    }
 
+$(function(){
+    var _st=1;
+    var _gt=1;
+    $("#ck1").parents().bind("click",function(){
+        _st*=-1;
+        console.log(_st);
+        if(_st==-1){
+            $(".masage tbody .icheckbox_flat-blue").removeClass("checked");
+        }else if(_st==1){
+            $(".masage tbody .icheckbox_flat-blue").addClass("checked");
+        }
+    });
+    $(".masage thead input").eq(1).click(function(){
+        _gt*=-1;
+        if(_gt==-1){
+            $(".masage tbody input:odd").prop("checked",false);
+        }else if (_gt==1){
+            $(".masage tbody input:odd").prop("checked",true);
+        }
+
+    });
 });
+
 
 
 
