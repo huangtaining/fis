b3209b325eb85a9f66b53140bebc33da5e1ab78d
yangqichao <qichao.yang@unspay.com>
2016-09-22 11:29:32 +0800

    change

--- modules/js/change.js
+++ modules/js/change.js
@@ -4,15 +4,18 @@ $(function(){
 
 
     function aanr(ii,oo,aa,ff){
-        var _s=ii.html();
-        aa.click(function(){
-            console.log(_s)
-        })
+        var _i=$(ii).html()||$(ii).val();
+        //aa.on(ff,function(){
+        //
+        //});
+        $(aa).on(ff,function(){
+            $(oo).val(_i)||$(oo).html(_i)
+        });
 
         //$(cc).on(ff,function(){
         //    $(oo).val($(ii).val())
         //})
     }
-    aanr($("#ii"),$("#oo"),$("#tt"),'click');
-    console.log("change")
-})
+    //第一个输入的，第二个输出位置，第三个触发的元素，第四个触发的事件
+    aanr("#ii","#oo","#tt",'click');
+});
