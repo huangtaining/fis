790bddf6e5c6660be0f0615ed5845c42ecbb9523
yangqichao <qichao.yang@unspay.com>
2016-09-30 15:21:15 +0800

    多选联系人

--- modules/widget/chooselxr/choose.js
+++ modules/widget/chooselxr/choose.js
@@ -3,17 +3,3 @@
  */
 var $ = require('jquery');
 console.log("sss");
-$("#bq1").click(function(){
-    console.log("sss");
-    $(".dxtit").removeClass('active');
-    $(this).addClass('active');
-    $(".m table").hide();
-    $("#ysb").show()
-});
-$("#bq2").click(function(){
-    console.log("ccc");
-    $(".dxtit").removeClass('active');
-    $(this).addClass('active');
-    $(".m table").hide();
-    $("#yh").show()
-});
